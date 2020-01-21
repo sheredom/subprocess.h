706a484ab7b349a0f9de3f84ae6d75bc86ea1073
Pascal Thomet <pthomet@gmail.com>
2020-01-21 14:31:33 +0100

    Failures tests: fool the optimizer

--- test/process_fail_divzero.c
+++ test/process_fail_divzero.c
@@ -1,10 +1,29 @@
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// This convoluted function returns 0
+// but will hopefuly not be optimzed away in release builds...
+int return_0_non_optimizable()
+{
+  char buffer[100];
+  long value = 62831853071796;
+  snprintf(buffer, 100, "%ld", value);
+  char *c = buffer;
+  int result = 0;
+  while (*c) {
+    result = result + *c - '\0';
+    c++;
+  }
+  return result - 738;
+}
 
 int main()
 {
-    int p = 42;
-    int q = p - 2 * 21;
-    int r = p / q; // this is an integer division by zero
-    printf("%d", r);
-    return 0;
+  int p = 42;
+  int q = return_0_non_optimizable();
+  int r = p / q; // this is an integer division by zero
+  printf("r=%d\n", r);
+  return 0;
 }
--- test/process_fail_stackoverflow.c
+++ test/process_fail_stackoverflow.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
  
+// This convoluted function returns 0
+// but will hopefuly not be optimzed away in release builds...
+int return_0_non_optimizable()
+{
+  char buffer[100];
+  long value = 62831853071796;
+  snprintf(buffer, 100, "%ld", value);
+  char *c = buffer;
+  int result = 0;
+  while (*c) {
+    result = result + *c - '\0';
+    c++;
+  }
+  return result - 738;
+}
+
+
 // this function is infinitely recursive and will cause a stack overflow
 int fun(int x) { 
   if (x == 1) 
       return 5;
   x = 6; 
-  fun(x);
+  if (return_0_non_optimizable() == 0)
+    fun(x);
   return x;
 } 
 
