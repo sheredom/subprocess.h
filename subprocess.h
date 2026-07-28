6cbe8fae74aeacf25536c9d02c78da44c848a00e
Thomas Klausner <wiz@gatalith.at>
2026-07-28 10:24:47 +0200

    Fix build on NetBSD.

--- subprocess.h
+++ subprocess.h
@@ -1205,7 +1205,7 @@ cleanup:
 
   // Set working directory
   if (process_cwd) {
-#if defined(__APPLE__) && MAC_OS_X_VERSION_MIN_REQUIRED >= 260000
+#if defined(__NetBSD__) || (defined(__APPLE__) && MAC_OS_X_VERSION_MIN_REQUIRED >= 260000)
     posix_error = posix_spawn_file_actions_addchdir(&actions, process_cwd);
 #else
 #if defined(__APPLE__) && defined(__clang__)
